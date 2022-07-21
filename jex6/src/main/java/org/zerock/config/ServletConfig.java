3986bf3f8397940132a18353679798ed3115faf6
Nohtaewon <gkgk713@naver.com>
2022-07-22 00:36:38 +0900

    자동 로그인, java 설정

---
+++ jex6/src/main/java/org/zerock/config/ServletConfig.java
@@ -0,0 +1,34 @@
+package org.zerock.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@EnableWebMvc
+@ComponentScan(basePackages= {"org.zerock.controller"})
+//어노테이션을 이용하는 시큐리티 설정
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+public class ServletConfig implements WebMvcConfigurer{
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		
+		InternalResourceViewResolver bean=new InternalResourceViewResolver();
+			bean.setViewClass(JstlView.class);
+			bean.setPrefix("/WEB-INF/views/");
+			bean.setSuffix(".jsp");
+			registry.viewResolver(bean);
+			
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+
+}
